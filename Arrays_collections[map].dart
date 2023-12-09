369bf4690ad63cf8ac46b70d40bb224e3ff5667f
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2023-12-09 12:34:43 +0530

    map

---
+++ Arrays_collections[map].dart
@@ -0,0 +1,18 @@
+//masp
+void main() {
+  var names = {
+1: "kaushal",
+2: "sanjay",//we can also change words to integer 
+3:"easwar"// we can also use first or words ib behalf of integers.
+  };
+  print(names[2]);
+  var name = Map();
+  name[1] = 'jack';
+  print(name[1]);
+  
+  var Names = {1:'jill',2: 'janson'};
+  print(Names[1]);
+
+  
+  
+  }
