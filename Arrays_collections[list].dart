3048eb2f7c4f7d73df9107f566bfd08ac43f7959
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2023-12-09 12:07:14 +0530

    Lists

---
+++ Arrays_collections[list].dart
@@ -0,0 +1,18 @@
+
+//list
+main(){
+List names = ['john','jacobs'];//if we are using var and erasing list any numbers like 10 or double 145.00 it will be also printed and the list will be changed to objects.
+print(names[0]);
+print(names[1]);
+print(names.length);
+for(var n in names){
+print(n);
+names[1] =  'starc';   
+}
+var names2 = [...names]; //here both names and names2 are pointing at jack and jill. not copied from names to names2
+
+
+}
+
+
+  
