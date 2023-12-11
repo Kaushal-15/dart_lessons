4234396e91b1cac435474dadf866059cc3ea1e50
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2023-12-11 19:45:45 +0530

    loops

---
+++ loops.dart
@@ -0,0 +1,29 @@
+
+
+void main() {
+  List names = ['kaushal', 'sanjay', 'easwar', 'kiruthika', 'savitha'];
+  List ages = [17, 11, 25, 24, 35];
+
+  for (var i = 0; i < names.length; i++) {
+    String name = names[i];
+    int age = ages[i];
+
+    print('Happy Birthday $name');
+    print('Itz $age');
+
+    
+    
+  }
+}
+ void main1() {
+  List Names = ['kaushal', 'sanjay', 'easwar', 'kiruthika', 'savitha'];
+  List Ages = [17, 11, 25, 24, 35];
+  int index = 0;
+  while (index > Names.length) {
+    String currentNames = Names[index];
+  var  currentAges = Ages[index];
+print('happy birthday $currentNames');
+print('itz $currentAges');
+    
+  }
+}
