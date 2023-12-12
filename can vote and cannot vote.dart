47b2976cbc19e463978834f655bf62cd038b485a
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2023-12-12 20:08:29 +0530

    extra questions

---
+++ can vote and cannot vote.dart
@@ -0,0 +1,11 @@
+void main() {
+   int age = 19; //if we provide other ages the result will differ
+
+   if (age<18) {
+    print("sorry,you cannot vote");
+
+   }else {
+    print("you can vote");
+   }
+  
+}
