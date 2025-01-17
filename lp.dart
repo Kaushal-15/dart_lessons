f1092d3161b2e073a4c28005c0e923022b99b779
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-17 13:56:42 +0530

    Create lp.dart

---
+++ lp.dart
@@ -0,0 +1,11 @@
+class laptop{
+  int price=0;
+  String brand = "kaushal";
+  
+}
+void main() {
+  laptop lap1 = laptop();
+  lap1.price=200000;
+  lap1.brand="hp";
+  print("the brand is ${lap1.brand}");
+}
